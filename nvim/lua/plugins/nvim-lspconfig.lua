195b88717da5a95c55007aee6ef029ab6f9e3e87
Zachary Selk <zrselk@gmail.com>
2022-12-26 12:06:27 -0700

    Switched from CCLS to Clangd

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -6,28 +6,28 @@ if not status_ok then
 end
 
 
-lspconfig.ccls.setup{
-  filetypes = { "c", "cpp", "hpp", "objc", "objcpp" },
-  init_options = {
-    cache = {directory = "/tmp/ccls-cache"},
-    compilationDatabaseDirectory = "cmake-build",
-    index = {
-      threads = 0,
-    };
-    clang = {
-      excludeArgs = { "-frounding-math"},
-    },
-  },
-  root_dir = 
-    function (dir)
-      local filename = dir:match("*.+/(.+)$");
-      if (filename == ".ccls") then
-        return true;
-      else
-        return false;
-      end
-    end,
-}
+--lspconfig.ccls.setup{
+--  filetypes = { "c", "cpp", "hpp", "objc", "objcpp" },
+--  init_options = {
+--    cache = {directory = "/tmp/ccls-cache"},
+--    compilationDatabaseDirectory = "cmake-build",
+--    index = {
+--      threads = 0,
+--    };
+--    clang = {
+--      excludeArgs = { "-frounding-math"},
+--    },
+--  },
+--  root_dir = 
+--    function (dir)
+--      local filename = dir:match("*.+/(.+)$");
+--      if (filename == ".ccls") then
+--        return true;
+--      else
+--        return false;
+--      end
+--    end,
+--}
 --lspconfig.jdtls.setup{}
 
 --lspconfig.denols.setup{}
@@ -55,7 +55,9 @@ lspconfig.pylsp.setup{
   }
 }
 
---lspconfig.clangd.setup{}
+lspconfig.clangd.setup{
+  cmd = { "clangd", "--log=verbose" },
+}
 
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
@@ -68,7 +70,8 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'ccls' }
+--local servers = { 'ccls' }
+local servers = { 'clangd' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
